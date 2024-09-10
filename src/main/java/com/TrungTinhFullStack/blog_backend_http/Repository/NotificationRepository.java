d01122bb04c3bf9b42eba277de32d2c4355aac36
VanTan22ab <izukumi5672@gmail.com>
2024-09-10 14:06:12 +0700

    Add findByUserId in NotificationRepostory

--- src/main/java/com/TrungTinhFullStack/blog_backend_http/Repository/NotificationRepository.java
+++ src/main/java/com/TrungTinhFullStack/blog_backend_http/Repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package com.TrungTinhFullStack.blog_backend_http.Repository;
 
 import com.TrungTinhFullStack.blog_backend_http.Entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List; // Cần import List
 
-public interface NotificationRepository extends JpaRepository<Notification,Long> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdAndIsReadFalse(Long userId);
 }
