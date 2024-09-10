551befbde53c989e622c7abf443c40595a9244ef
VanTan22ab <izukumi5672@gmail.com>
2024-09-10 14:03:07 +0700

    Create getNotifications in NotificationController

--- src/main/java/com/TrungTinhFullStack/blog_backend_http/Controller/NotificationController.java
+++ src/main/java/com/TrungTinhFullStack/blog_backend_http/Controller/NotificationController.java
@@ -3,10 +3,9 @@ package com.TrungTinhFullStack.blog_backend_http.Controller;
 import com.TrungTinhFullStack.blog_backend_http.Entity.Notification;
 import com.TrungTinhFullStack.blog_backend_http.Repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notifications")
@@ -14,6 +13,11 @@ public class NotificationController {
 
     @Autowired
     private NotificationRepository notificationRepository;
+
+    @GetMapping("/{userId}")
+    public List<Notification> getNotifications(@PathVariable Long userId) {
+        return notificationRepository.findByUserIdAndIsReadFalse(userId);
+    }
 
     @PostMapping("/{id}/mark-read")
     public void markAsRead(@PathVariable Long id) {
